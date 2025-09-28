85e3eff5b0aa75f63380281c9ebffde9f8979986
Leticia Santiago e Silva <santiagoleticia68@gmail.com>
2025-09-28 22:14:08 +0000

    28

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,24 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { createBrowserRouter, RouterProvider} from 'react-router-dom'
+import Home from './routes/Home/index.tsx'
+import Error from './routes/Error/index.tsx'
+const router= createBrowserRouter([
+  {
+    path:'/',
+    element:<App/>,
+    errorElement:<Error/>,
+    children:[{
+      path:"/",
+      element:<Home/>
+    }]
+  }
+])
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
